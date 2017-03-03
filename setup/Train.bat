78133b25f7d5760670a7ccb6202fb59dea9fe43c
Junyi Hu <Accelouch@gmail.com>
2017-03-03 20:35:01 +0800

    Adandon AlexNet

---
+++ setup/Train.bat
@@ -0,0 +1,2 @@
+$(Your_caffe_root)\caffe.exe train --solver=D:\Research\LoveLiveFaceRecognition\model\solver.prototxt
+pause
---
+++ setup/convertimage2ldb_train.bat
@@ -0,0 +1,3 @@
+SET GLOG_logtostderr=1
+$(Your_caffe_root)\convert_imageset.exe --resize_height=132 --resize_width=132 --shuffle --backend="leveldb" D:\Research\LoveLiveFaceRecognition\ D:\Research\LoveLiveFaceRecognition\label\train.txt D:\Research\LoveLiveFaceRecognition\trainLevelDB
+pause
